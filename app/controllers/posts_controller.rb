2e31fb829ffd7f610a2953342fcdfd64ccc4629e
Swidjit <alex@swidjit.com>
2015-01-18 22:24:44 -0500

    users can add posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
-
+  respond_to :js
   def show
     @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      render :partial => 'create.js'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content_type, :content)
   end
 end
