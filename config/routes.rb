06e52c4a0f4efd88413513323e5f98f52df666c5
Swidjit <alex@swidjit.com>
2015-01-18 21:32:22 -0500

    adding facebook url share for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users,
     :controllers => { :registrations => "registrations" }
+
+  resources :posts
+
   root :to => 'pages#home'
   get 'pages/:page_name' => 'pages#index', :as => :pages
 end
