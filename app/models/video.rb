8ef87f9154633077d73a8747ae515f1c9a451909
Swidjit <alex@swidjit.com>
2015-01-26 22:32:25 -0500

    new header

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,13 +1,14 @@
-class Image < Post
+class Video < Post
   # This method associates the attribute ":photo" with a file attachment
-  has_attached_file :photo, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
+  has_attached_file :video,
+    :storage => :s3,
+    :path => "/video/:id/:filename",
+    :s3_headers =>  { "Content-Type" => "video/mp4" }
 
   # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :video,
+     :content_type => ['video/mp4'],
+     :message => "Sorry, this site currently only supports MP4 video"
 
 
 end
